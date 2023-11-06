6716720d189fe2b5376e3d109cfaff4c87362944
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-06 09:26:09 +0700

    refactor: use default package for refresh (#91)

--- lib/common_widgets/base_refresh/presentation/base_refresh.dart
+++ lib/common_widgets/base_refresh/presentation/base_refresh.dart
@@ -1,8 +1,4 @@
-import 'package:daily_mind/constants/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:get/utils.dart';
-import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart'
-    hide RefreshCallback;
 
 class BaseRefresh extends StatelessWidget {
   final RefreshCallback onRefresh;
@@ -16,16 +12,9 @@ class BaseRefresh extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: kToolbarHeight),
-      child: LiquidPullToRefresh(
-        onRefresh: onRefresh,
-        showChildOpacityTransition: false,
-        springAnimationDurationInMilliseconds: defaultDuration.inMilliseconds,
-        backgroundColor: context.theme.primaryColor,
-        color: context.theme.colorScheme.background.withOpacity(0.8),
-        child: child,
-      ),
+    return RefreshIndicator(
+      onRefresh: onRefresh,
+      child: child,
     );
   }
 }
