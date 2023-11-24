d6f7670e2323dc20408f62d77794f71d7c710da1
Andy <10527554+andyngdz@users.noreply.github.com>
2023-11-24 15:30:37 +0700

    Add more sounds, reduce sizes (#142)
    * refactor: reduce app size by using less audios
    
    * feat: added more sounds, and resize images

--- lib/constants/offline_audio_sources.dart
+++ lib/constants/offline_audio_sources.dart
@@ -8,37 +8,32 @@ OfflineAudioSources offlineAudioSources = {
   ],
   'fire': [
     'assets/audios/fires/fire-1.mp3',
-    'assets/audios/fires/fire-2.mp3',
-    'assets/audios/fires/fire-3.mp3',
   ],
   'rain': [
     'assets/audios/rains/rain-1.mp3',
-    'assets/audios/rains/rain-2.mp3',
-    'assets/audios/rains/rain-3.mp3',
   ],
   'forest': [
     'assets/audios/forests/forest-1.mp3',
-    'assets/audios/forests/forest-2.mp3',
-    'assets/audios/forests/forest-3.mp3',
   ],
   'space': [
     'assets/audios/spaces/space-1.mp3',
-    'assets/audios/spaces/space-2.mp3',
-    'assets/audios/spaces/space-3.mp3'
   ],
   'water': [
     'assets/audios/waters/water-1.mp3',
-    'assets/audios/waters/water-2.mp3',
-    'assets/audios/waters/water-3.mp3',
+  ],
+  "wave": [
+    'assets/audios/waves/wave-1.mp3',
   ],
   'wind': [
     'assets/audios/winds/wind-1.mp3',
-    'assets/audios/winds/wind-2.mp3',
-    'assets/audios/winds/wind-3.mp3',
+  ],
+  "catPurring": [
+    'assets/audios/cat-purrings/cat-purring-1.mp3',
+  ],
+  'deltaWave': [
+    'assets/audios/delta-waves/delta-wave-1.mp3',
   ],
   'whiteNoise': [
     'assets/audios/white-noises/white-noise-1.mp3',
-    'assets/audios/white-noises/white-noise-2.mp3',
-    'assets/audios/white-noises/white-noise-3.mp3',
   ],
 };
--- lib/constants/offline_audios.dart
+++ lib/constants/offline_audios.dart
@@ -4,43 +4,51 @@ final List<AudioOffline> offlineAudios = [
   AudioOffline(
     id: 'rain',
     name: 'rain',
-    description: 'rainDescription',
     image: 'assets/images/rain-forest.png',
   ),
   AudioOffline(
     id: 'forest',
     name: 'forest',
-    description: 'forestDescription',
     image: 'assets/images/forest-with-birds.png',
   ),
   AudioOffline(
     id: 'wind',
     name: 'wind',
-    description: 'windDescription',
     image: 'assets/images/wind-and-peaceful.png',
   ),
   AudioOffline(
     id: 'fire',
     name: 'fire',
-    description: 'fireDescription',
     image: 'assets/images/firecamp.png',
   ),
   AudioOffline(
     id: 'water',
     name: 'water',
-    description: 'waterDescription',
     image: 'assets/images/water.png',
+  ),
+  AudioOffline(
+    id: 'wave',
+    name: 'wave',
+    image: 'assets/images/wave.png',
   ),
   AudioOffline(
     id: 'space',
     name: 'space',
-    description: 'spaceDescription',
     image: 'assets/images/sky.png',
   ),
   AudioOffline(
     id: 'whiteNoise',
     name: 'whiteNoise',
-    description: 'whiteNoiseDescription',
     image: 'assets/images/white-noise.png',
+  ),
+  AudioOffline(
+    id: 'deltaWave',
+    name: 'deltaWave',
+    image: 'assets/images/delta-wave.png',
+  ),
+  AudioOffline(
+    id: 'catPurring',
+    name: 'catPurring',
+    image: 'assets/images/cat-purring.png',
   ),
 ];
